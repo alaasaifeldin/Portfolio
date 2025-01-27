eba3212f3d2dc355709b00609b114e5f5fbe2829
MahmoudGado1 <mahmoudgado444@gmail.com>
2025-01-27 15:50:37 +0200

    Updated next.config.js for image handling

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    // Enables Next.js image optimization
-    unoptimized: true, // Set to 'true' only if you want to disable image optimization for local testing
-  },
 };
 
 export default nextConfig;
